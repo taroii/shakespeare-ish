bcd31a28501a875f9bd401adc5d3b0243e78a321
Taro <114524578+taroii@users.noreply.github.com>
2023-04-11 15:21:23 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/taroii/sonnet-generator"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer ****INSERT CODE HERE****"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
